0b62a87689d0456b2ba0408b3cf2d8091a67a550
MuhilKumaran <137814562+MuhilKumaran@users.noreply.github.com>
2024-03-19 19:10:39 +0530

    Time: 48 ms (67.43%) | Memory: 73.3 MB (67.62%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -4,8 +4,7 @@ class Solution {
          int i;
          int count=0;
         int prev=inter[0][1];
-        for(i=0;i<inter.length;i++)
-            System.out.println(inter[i][0]+"  "+inter[i][1]);
+        
          for(i=1;i<inter.length;i++)
          {
             if(inter[i][0]<prev)
