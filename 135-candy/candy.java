0d576e3932d8399265cba77a2441c311dc33883e
MuhilKumaran <137814562+MuhilKumaran@users.noreply.github.com>
2024-03-19 11:45:47 +0530

    Time: 3 ms (88.19%) | Memory: 45.8 MB (24.88%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int candy(int[] rate) {
+        int res[]= new int[rate.length];
+        int n=rate.length;
+        int i;
+        for(i=0;i<n;i++)
+        {
+            res[i]=1;
+        }
+        int count=0;
+        for(i=0;i<rate.length-1;i++)
+        {
+            if(rate[i+1]>rate[i])
+            {
+                res[i+1]=1+res[i];
+            }
+        }
+        for(i=n-1;i>0;i--)
+        {
+            if(rate[i-1]>rate[i])
+            {
+                res[i-1]=Math.max(res[i-1],1+res[i]);
+            }
+        }
+        for(int val:res)
+            count+=val;
+        return count;
+    }
+}
