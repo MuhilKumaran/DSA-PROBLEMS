d5360904b78d6f07004b849b616bcf18e5cbfee0
MuhilKumaran <137814562+MuhilKumaran@users.noreply.github.com>
2024-03-20 12:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (36.74%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,22 +1,24 @@
 class Solution {
-    static int getAns(int nums[],int ind,int dp[])
-    {
-        if(ind>=nums.length)
-            return 0;
-        if(dp[ind]!=-1)
-            return dp[ind];
-        int pick= nums[ind]+getAns(nums,ind+2,dp);
-        int notpick= getAns(nums,ind+1,dp);;
-        dp[ind]=  Math.max(pick,notpick);
-        return dp[ind];
-    }
+    // static int getAns(int nums[],int ind,int dp[])
+    // {
+    //     if(ind>=nums.length)
+    //         return 0;
+    //     if(dp[ind]!=-1)
+    //         return dp[ind];
+    //     int pick= nums[ind]+getAns(nums,ind+2,dp);
+    //     int notpick= getAns(nums,ind+1,dp);;
+    //     dp[ind]=  Math.max(pick,notpick);
+    //     return dp[ind];
+    // }
     public int rob(int[] nums) {
-        int dp[] = new int[nums.length];
+        int dp[] = new int[nums.length+1];
         int i;
-        for(i=0;i<nums.length;i++)
+        Arrays.fill(dp,0);
+        dp[1]=nums[0];
+        for(i=2;i<=nums.length;i++)
         {
-            dp[i]=-1;
+            dp[i]=Math.max(dp[i-2]+nums[i-1],dp[i-1]);
         }
-        return getAns(nums,0,dp);
+        return dp[nums.length];
     }
 }
