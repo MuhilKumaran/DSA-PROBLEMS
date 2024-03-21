99c94d94cb7b46b9202d620976e515d14f395c26
MuhilKumaran <137814562+MuhilKumaran@users.noreply.github.com>
2024-03-21 20:30:32 +0530

    Time: 1 ms (99.96%) | Memory: 42.4 MB (95.54%) - LeetSync

--- 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.java
+++ 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.java
@@ -1,21 +1,36 @@
 class Solution {
     public int sumOfEncryptedInt(int[] nums) {
-        int sum=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            String num = nums[i]+"";
-            // System.out.println(num);
-            int max=0;
-            int j;
-            for(j=0;j<num.length();j++)
-            {
-                max=Math.max(max,num.charAt(j)-'0');
-            }
-            String temp=max+"";
-            String res=temp.repeat(j);
-            // System.out.println(res);
-            sum+=Integer.parseInt(res);
+        // int sum=0;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     String num = nums[i]+"";
+        //     // System.out.println(num);
+        //     int max=0;
+        //     int j;
+        //     for(j=0;j<num.length();j++)
+        //     {
+        //         max=Math.max(max,num.charAt(j)-'0');
+        //     }
+        //     String temp=max+"";
+        //     String res=temp.repeat(j);
+        //     // System.out.println(res);
+        //     sum+=Integer.parseInt(res);
             
+        // }
+        // return sum;
+        int sum = 0;
+        for (int num : nums) {
+            if (num <= 99) {
+                if (num <= 9) {
+                    sum += num;
+                } else {
+                    sum += 11 * Math.max(num % 10, num / 10);
+                }
+            } else if (num <= 999) {
+                sum += 111 * Math.max(num % 10, Math.max((num / 10) % 10, num / 100));
+            } else {
+                sum += 1111;
+            }
         }
         return sum;
     }
