82160492494ea0f67f89289f42c912df13af48ea
MuhilKumaran <137814562+MuhilKumaran@users.noreply.github.com>
2024-03-21 09:42:53 +0530

    Time: 22 ms (43.15%) | Memory: 53 MB (6.23%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,32 +1,53 @@
 class Solution 
 {
     static int dp[][];
-    static int getLength(String t1,String t2,int ind1,int ind2)
-    {
-        if(ind1>=t1.length() || ind2>=t2.length())
-        {
-            return 0;
-        }
-        if(dp[ind1][ind2]!=-1)
-            return dp[ind1][ind2];
-        if(t1.charAt(ind1)==t2.charAt(ind2))
-        {
-            return  dp[ind1][ind2] = 1+getLength(t1,t2,ind1+1,ind2+1);
-        }
-        else
-        {
-            int q1 = getLength(t1,t2,ind1,ind2+1);
-            int q2 = getLength(t1,t2,ind1+1,ind2);
-            return dp[ind1][ind2] = Math.max(q1,q2); 
-        }
-    }
+    // static int getLength(String t1,String t2,int ind1,int ind2)
+    // {
+    //     if(ind1>=t1.length() || ind2>=t2.length())
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[ind1][ind2]!=-1)
+    //         return dp[ind1][ind2];
+    //     if(t1.charAt(ind1)==t2.charAt(ind2))
+    //     {
+    //         return  dp[ind1][ind2] = 1+getLength(t1,t2,ind1+1,ind2+1);
+    //     }
+    //     else
+    //     {
+    //         int q1 = getLength(t1,t2,ind1,ind2+1);
+    //         int q2 = getLength(t1,t2,ind1+1,ind2);
+    //         return dp[ind1][ind2] = Math.max(q1,q2); 
+    //     }
+    // }
     public int longestCommonSubsequence(String text1, String text2) {
-        dp = new int[text1.length()][text2.length()];
-        int i;
-        for(i=0;i<dp.length;i++)
+        dp = new int[text1.length()+1][text2.length()+1];
+        int i,j;
+        // for(i=0;i<dp.length;i++)
+        // {
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return getLength(text1,text2,0,0);
+        int m=text1.length();
+        int n=text2.length();
+        for(i=0;i<=m;i++)
         {
-            Arrays.fill(dp[i],-1);
+            for(j=0;j<=n;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+                }
+                else if(text1.charAt(i-1)==text2.charAt(j-1))
+                {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
-        return getLength(text1,text2,0,0);
+        return dp[m][n];
     }
 }
